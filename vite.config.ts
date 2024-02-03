fab43e0963bf0b856d5a7dfd99435baf7114aca0
aureate <aureate16@gmail.com>
2024-02-04 00:59:37 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-   base:"TypeScript_Hotel_Project/",
+   base:"/TypeScript_Hotel_Project/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // esbuild: {
-  //   jsxFactory: 'h',
-  //   jsxFragment: 'Fragment',
-  //   jsxInject: `import { h, Fragment } from 'vue'`,
-  // },
   
 })
